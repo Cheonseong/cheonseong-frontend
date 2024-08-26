2ac3cdd08bfe9b975657470d447b360890cac71a
Yechan Lee <yechan@nineright.com>
2024-08-26 21:11:28 +0900

    feat: registerCategoryModal - similarity check using jaccard distance

--- src/components/common/modal/RegisterCategoryModal.tsx
+++ src/components/common/modal/RegisterCategoryModal.tsx
@@ -15,8 +15,15 @@ const RegisterCategoryModal = forwardRef<RegisterCategoryModalRef, RegisterCateg
   (props, ref) => {
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
     const [nameInputValue, setNameInputValue] = useState<string>(props.initialInputValue || '');
-    const [messageValue, setMessageValue] = useState<string>('오타가 없는지 잘 확인해주세요.');
+    const [messageValue, setMessageValue] = useState<string>(
+      '기존 분류명들과 의미가 겹치지 않는지 확인해주세요.',
+    );
     const [isConfirmButtonEnabled, setIsConfirmButtonEnabled] = useState<boolean>(true);
+
+    const [similarNames, setSimilarNames] = useState<string[]>([]);
+    const [notSimilarNames, setNotSimilarNames] = useState<string[]>(
+      categorySampleData.map((record) => record.value),
+    );
 
     const openModal = (value: string) => {
       setNameInputValue(value);
@@ -33,12 +40,37 @@ const RegisterCategoryModal = forwardRef<RegisterCategoryModalRef, RegisterCateg
       closeModal();
     };
 
+    const getJaccardDistance = (a: string, b: string) => {
+      const [aTokens, bTokens] = [a.trim().split(''), b.trim().split('')];
+      const union = [...new Set([...aTokens, ...bTokens])];
+      const intersection = aTokens.filter((char) => bTokens.includes(char));
+
+      return intersection.length / union.length;
+    };
+
+    const getSimilarNames = (pivotValue: string) => {
+      return categorySampleData
+        .map((record) => getJaccardDistance(record.value, pivotValue))
+        .map((jaccard, i) => (jaccard >= 0.2 ? categorySampleData[i].value : null))
+        .filter((x) => x) as string[];
+    };
+
+    const getNotSimilarNames = (pivotValue: string) => {
+      return categorySampleData
+        .map((record) => getJaccardDistance(record.value, pivotValue))
+        .map((jaccard, i) => (jaccard < 0.2 ? categorySampleData[i].value : null))
+        .filter((x) => x) as string[];
+    };
+
     const handleInputValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setSimilarNames(getSimilarNames(e.target.value));
+      setNotSimilarNames(getNotSimilarNames(e.target.value));
+
       if (categorySampleData.filter((record) => record.value === e.target.value).length !== 0) {
         setMessageValue('⚠ 중복된 분류명을 등록할 수 없습니다.');
         setIsConfirmButtonEnabled(false);
       } else {
-        setMessageValue('오타가 없는지 잘 확인해주세요.');
+        setMessageValue('기존 분류명들과 의미가 겹치지 않는지 확인해주세요.');
         setIsConfirmButtonEnabled(true);
       }
 
@@ -68,13 +100,34 @@ const RegisterCategoryModal = forwardRef<RegisterCategoryModalRef, RegisterCateg
                       type="text"
                       value={nameInputValue}
                       onChange={(e) => handleInputValueChange(e)}
-                      placeholder="홍길동"
+                      placeholder="십일조"
                       className="mx-3 mb-4 w-56 rounded border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
                   </span>
                 </div>
-                <p className="pb-6 text-sm">{messageValue}</p>
-                <div className="flex justify-end space-x-2">
+
+                <p className="pb-4 text-sm">{messageValue}</p>
+
+                <div className="w-full">
+                  {similarNames.map((value, i) => (
+                    <span
+                      key={i}
+                      className="mx-1 whitespace-nowrap rounded bg-sky-600 px-2 py-0.5 text-xs font-semibold text-white"
+                    >
+                      {value}
+                    </span>
+                  ))}
+                  {notSimilarNames.map((value, i) => (
+                    <span
+                      key={i}
+                      className="mx-1 whitespace-nowrap rounded bg-gray-400 px-2 py-0.5 text-xs font-semibold text-white"
+                    >
+                      {value}
+                    </span>
+                  ))}
+                </div>
+
+                <div className="mt-6 flex justify-end space-x-2">
                   <button
                     onClick={closeModal}
                     className="rounded bg-gray-300 px-4 py-2 text-gray-700 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400"
