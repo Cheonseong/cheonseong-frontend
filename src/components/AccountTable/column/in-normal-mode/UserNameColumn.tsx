c6be843d4b0fd33f6e377fd7eedf3c798339bfc1
Yechan Lee <yechan@nineright.com>
2024-08-27 14:22:37 +0900

    feat: add error signs to UserNameColumn

--- src/components/AccountTable/column/in-normal-mode/UserNameColumn.tsx
+++ src/components/AccountTable/column/in-normal-mode/UserNameColumn.tsx
@@ -1,12 +1,31 @@
-import { AccountRecord } from '../../data/AccountData';
+import { AccountRecord, userSampleData } from '../../data/AccountData';
 import { accountTableCellStyle } from '../../AccountTableBody';
+import CompactErrorSign from '../../../common/warning-sign/CompactWarningSign';
 
 interface UserNameColumnProps {
   record: AccountRecord;
 }
 
 const UserNameColumn = ({ record }: UserNameColumnProps) => {
-  return <td className={accountTableCellStyle}>{record.userName}</td>;
+  return (
+    <>
+      <td className={accountTableCellStyle}>
+        {record.userName === '' ? (
+          <>
+            <CompactErrorSign fontSize="text-base" errorMessage="이름이 선택되지 않았습니다." />
+            <span>{record.userName}</span>
+          </>
+        ) : userSampleData.filter((x) => x.value === record.userName).length === 0 ? (
+          <>
+            <CompactErrorSign fontSize="text-base" errorMessage="등록되지 않은 이름입니다." />
+            <span>{record.userName}</span>
+          </>
+        ) : (
+          record.userName
+        )}
+      </td>
+    </>
+  );
 };
 
 export default UserNameColumn;
---
+++ src/components/common/warning-sign/CompactWarningSign.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+interface CompactErrorSignProps {
+  fontSize:
+    | 'text-xs'
+    | 'text-sm'
+    | 'text-base'
+    | 'text-lg'
+    | 'text-xl'
+    | 'text-2xl'
+    | 'text-3xl'
+    | 'text-4xl'
+    | 'text-5xl'
+    | 'text-6xl'
+    | 'text-7xl'
+    | 'text-8xl'
+    | 'text-9xl';
+  errorMessage: string;
+}
+
+const CompactErrorSign = ({ fontSize, errorMessage }: CompactErrorSignProps) => {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <span
+      className={
+        'relative mx-2 break-keep rounded bg-rose-500 px-2 py-0.5 font-extrabold text-white select-none ' +
+        fontSize
+      }
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      !
+      {hovered && (
+        <span className="absolute bottom-full left-0 mb-2 whitespace-nowrap rounded-md bg-rose-50 p-2 font-semibold text-rose-600 shadow-lg">
+          {errorMessage}
+        </span>
+      )}
+    </span>
+  );
+};
+
+export default CompactErrorSign;
